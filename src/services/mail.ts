3dd29b79a5d299f5cd8974c95cde50ac73861230
Megabyte-webdev <afolabimubarak18@gmail.com>
2026-03-31 17:18:04 +0100

    new mail service provider added

--- src/services/mail.ts
+++ src/services/mail.ts
@@ -1,35 +1,36 @@
-import * as nodemailer from "nodemailer";
+import { Resend } from "resend";
+const resend = new Resend("re_f1nXWkCT_DWngaeWEH6mvzu4CQ74B6Nkp");
 
-const transporter: any = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: 587,
-  secure: false, // true for 465, false for 587
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  pool: true,
-  logger: process.env.NODE_ENV !== "production", // log only in dev
-  debug: process.env.NODE_ENV !== "production",
-});
+// const transporter: any = nodemailer.createTransport({
+//   host: process.env.EMAIL_HOST,
+//   port: 587,
+//   secure: false, // true for 465, false for 587
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+//   pool: true,
+//   logger: process.env.NODE_ENV !== "production", // log only in dev
+//   debug: process.env.NODE_ENV !== "production",
+// });
 
-export const verifyTransporter = async () => {
-  try {
-    await transporter.verify();
-    await sendEmail(
-      "afolabimubarak18@gmail.com",
-      "Test Email",
-      "<p>Hello World</p>",
-    );
-    console.log("Mail server is ready to take our messages");
-  } catch (err) {
-    console.error(" Failed to verify transporter:", err);
-  }
-};
+// export const verifyTransporter = async () => {
+//   try {
+//     await transporter.verify();
+//     await sendEmail(
+//       "afolabimubarak18@gmail.com",
+//       "Test Email",
+//       "<p>Hello World</p>",
+//     );
+//     console.log("Mail server is ready to take our messages");
+//   } catch (err) {
+//     console.error(" Failed to verify transporter:", err);
+//   }
+// };
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
   try {
-    const info = await transporter.sendMail({
+    const info = await resend.emails.send({
       from: `"OOU IRAP Portal" <${process.env.EMAIL_USER}>`,
       to,
       subject,
