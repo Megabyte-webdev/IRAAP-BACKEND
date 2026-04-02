2b142d56043fa22222466e1271be9d29b9dc5edc
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2026-04-02 03:25:41 +0100

    Update email.worker.ts

--- src/workers/email.worker.ts
+++ src/workers/email.worker.ts
@@ -4,7 +4,7 @@ import { getEmailData } from "../utils/email/engine.js";
 import { sendEmail } from "../services/mail.js";
 import { redisOptions } from "../config/redis.js";
 
-const workerConnection = new Redis(process.env.REDIS_URL, redisOptions);
+const workerConnection = new Redis(process.env.REDIS_URL as string, redisOptions);
 
 // 2. Add an error listener so it doesn't fail silently
 workerConnection.on('error', (err) => {
